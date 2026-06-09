441626d83d90574188cf3c270ad33a2495041501
Kong Seng <KongSengHero@gmail.com>
2026-06-09 14:49:34 +0700

    Added Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from 'react-router-dom'; 
+import { Routes, Route, Router } from 'react-router-dom'; 
 import Home from './pages/Home'; 
 import About from './pages/About'; 
 import Contact from './pages/Contact'; 
+import NavigationHeader from './components/NavigationHeader';
 
 
 
@@ -9,10 +10,20 @@ import Contact from './pages/Contact';
 
 export default function App() { 
     return ( 
-        <Routes>
-            <Route path="/" element={ <Home /> } />
-            <Route path="/contact" element={ <Contact /> } />
-            <Route path="/about" element={ <About /> } />
-        </Routes>
+        <>
+            <NavigationHeader />
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="/contact" element={ <Contact /> } />
+                <Route path="/about" element={ <About /> } />
+                <Route path='*' element={ 
+                    <div className="z-50 fixed inset-0 flex items-center justify-center p-4 bg-neutral-900/95 backdrop-blur-sm text-white">
+                        <div className="text-center flex flex-col items-center">
+                            <h2 className="text-xl font-bold mt-4">No Page Found...</h2>
+                        </div>
+                    </div>
+                } />
+            </Routes>
+        </>
     )
 }
